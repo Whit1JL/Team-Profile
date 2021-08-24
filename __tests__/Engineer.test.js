b70103c16c4ea765978aae15809651fdc11a6e84
Jessica White <jessica2714white@gmail.com>
2021-08-23 21:09:15 -0500

    created tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require("../lib/Engineer");
+
+test("Can set GitHub account via constructor", () => {
+  const testValue = "GithubUser";
+  const e = new Engineer("Ben", 1, "test@test.com", testValue);
+  expect(e.getGithub).toBe(testValue);
+});
+
+test("getRole() should return \"Engineer\"", () => {
+  const testValue = "Engineer";
+  const e = new Engineer("Ben", 1, "test@test.com", "GithubUser");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get GitHub username via getGithub()", () => {
+  const testValue = "GithubUser";
+  const e = new Engineer("Ben", 1, "test@test.com", testValue);
+  expect(e.getGithub()).toBe(testValue);
+});
